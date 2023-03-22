845d5e7eb7723a6f089114eb07010a07e8d8accc
Tom Artale <t.artale@f5.com>
2023-03-21 23:03:07 -0600

    got folded rows to work, but not display correctly yet

--- webui/src/components/ShowListing.tsx
+++ webui/src/components/ShowListing.tsx
@@ -12,7 +12,7 @@ import "./ShowListing.css";
 import {v4 as uuidv4} from "uuid";
 
 export interface Show {
-  id?: number;
+  id: string;
   kind: string;
   title: string;
   recordedOn: Date;
@@ -70,13 +70,7 @@ export const getImageFileForShow = (show: Show, open: boolean): string => {
   }
 };
 
-function Row(props: {show: Show}) {
-  const {show} = props;
-  console.log(show);
-  const [open, setOpen] = React.useState(false);
-
-  const episodeCount = (show as Series).episodes?.length || 0;
-  const episodeCountLabel = episodeCount > 1 ? `[${episodeCount}]` : "";
+const parseRecordingDate = (show: Show) => {
   const dayOfWeek = recordedOn(show)?.toLocaleDateString("en-US", {
     weekday: "short",
   });
@@ -85,15 +79,26 @@ function Row(props: {show: Show}) {
     day: "2-digit",
   });
 
-  const ShowIcon = (props: {show: Show}) => {
-    const imageFile: string = getImageFileForShow(show, open);
+  return {dayOfWeek, monthDay};
+};
 
-    return (
-      <TableCell>
-        <img src={imageFile} style={{width: "3rem", height: "3rem"}} alt="" />
-      </TableCell>
-    );
-  };
+function IconCell(props: {show: Show; open: boolean}) {
+  const {show, open} = props;
+  const imageFile: string = getImageFileForShow(show, open);
+
+  return (
+    <TableCell>
+      <img src={imageFile} style={{width: "3rem", height: "3rem"}} alt="" />
+    </TableCell>
+  );
+}
+
+function Row(props: {show: Show}) {
+  const {show} = props;
+  const [open, setOpen] = React.useState(false);
+  const episodeCount = (show as Series).episodes?.length || 0;
+  const episodeCountLabel = episodeCount > 1 ? `[${episodeCount}]` : "";
+  const {dayOfWeek, monthDay} = parseRecordingDate(show);
 
   return (
     <React.Fragment>
@@ -101,62 +106,58 @@ function Row(props: {show: Show}) {
         sx={{"& > *": {borderBottom: "unset"}}}
         onClick={() => setOpen(!open)}
       >
-        <ShowIcon show={show} />
+        <IconCell show={show} open={open} />
         <TableCell>
           {show.title} {episodeCountLabel}
         </TableCell>
         <TableCell>{dayOfWeek}</TableCell>
         <TableCell>{monthDay}</TableCell>
       </TableRow>
+      <EpisodeRows show={show} open={open} />
+    </React.Fragment>
+  );
+}
+
+function EpisodeRow(props: {key: string; show: Show}) {
+  const {key, show} = props;
+  const {dayOfWeek, monthDay} = parseRecordingDate(show);
+
+  return (
+    <TableRow key={key} sx={{"& > *": {borderBottom: "unset"}}}>
+      <IconCell show={show} open={false} />
+      <TableCell>{show.title}</TableCell>
+      <TableCell>{dayOfWeek}</TableCell>
+      <TableCell>{monthDay}</TableCell>
+    </TableRow>
+  );
+}
+
+function EpisodeRows(props: {show: Show; open: boolean}) {
+  const {show, open} = props;
+
+  if (
+    !open ||
+    show.kind !== "series" ||
+    (show as Series).episodes.length <= 1
+  ) {
+    return <React.Fragment />;
+  }
+
+  return (
+    <React.Fragment>
       <TableRow>
         <Collapse in={open} timeout="auto" unmountOnExit>
           <Box sx={{margin: 1}}>
             <Table className="showListingTable">
               <TableBody>
                 {(show as Series).episodes?.map((episode) => (
-                  <Row key={episode.id} show={{...show, ...episode}} />
+                  <EpisodeRow key={episode.id} show={{...show, ...episode}} />
                 ))}
               </TableBody>
             </Table>
           </Box>
         </Collapse>
       </TableRow>
-
-      {/* <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow> */}
     </React.Fragment>
   );
 }
@@ -176,6 +177,8 @@ export default function ShowListing() {
           episodes: obj.episodes?.map(
             (episode: Episode): Episode => ({
               ...episode,
+              kind: "episode",
+              id: obj.id || uuidv4(),
               recordedOn: new Date(episode.recordedOn),
             })
           ),
@@ -197,3 +200,39 @@ export default function ShowListing() {
     </TableContainer>
   );
 }
+
+// {/* <TableRow>
+//   <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+//     <Collapse in={open} timeout="auto" unmountOnExit>
+//       <Box sx={{ margin: 1 }}>
+//         <Typography variant="h6" gutterBottom component="div">
+//           History
+//         </Typography>
+//         <Table size="small" aria-label="purchases">
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>Date</TableCell>
+//               <TableCell>Customer</TableCell>
+//               <TableCell align="right">Amount</TableCell>
+//               <TableCell align="right">Total price ($)</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {row.history.map((historyRow) => (
+//               <TableRow key={historyRow.date}>
+//                 <TableCell component="th" scope="row">
+//                   {historyRow.date}
+//                 </TableCell>
+//                 <TableCell>{historyRow.customerId}</TableCell>
+//                 <TableCell align="right">{historyRow.amount}</TableCell>
+//                 <TableCell align="right">
+//                   {Math.round(historyRow.amount * row.price * 100) / 100}
+//                 </TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </Box>
+//     </Collapse>
+//   </TableCell>
+// </TableRow> */}
