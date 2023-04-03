5de903239c217596c073988079abe76ef0200cb9
Tom Artale <t.artale@f5.com>
2023-04-03 15:18:53 -0600

    latest common

--- webui/src/components/ShowRow.tsx
+++ webui/src/components/ShowRow.tsx
@@ -4,6 +4,10 @@ import TableHead from "@mui/material/TableHead";
 import React from "react";
 import { getImageFileForShow, getTitleExtension, recordedOn } from "./showListingHelpers";
 import { ShowKind, Series } from "./ShowListing";
+import IconButton from "@mui/material/IconButton";
+import UnfoldMoreSharpIcon from '@mui/icons-material/UnfoldMoreSharp';
+import DownloadSharpIcon from '@mui/icons-material/DownloadSharp';
+import { makeStyles } from '@mui/material/styles';
 
 export function ShowHeader(props: any) {
 
@@ -14,6 +18,7 @@ export function ShowHeader(props: any) {
         <TableCell width={"20%"} >Title</TableCell>
         <TableCell>Description</TableCell>
         <TableCell width={"10%"} >Recorded On</TableCell>
+        <TableCell width={"10%"} ></TableCell>
       </TableRow>
     </TableHead>
   )
@@ -31,6 +36,7 @@ export function ShowRow(props: any) {
         <TitleCell width={"20%"} show={show} />
         <DescriptionCell show={show} />
         <RecordedOnCell width={"10%"} show={show} />
+        <ActionCell width={"10%"} show={show} />
       </TableRow>
       <EpisodeRows show={show} open={open} />
     </React.Fragment>
@@ -50,7 +56,6 @@ function EpisodeRows(props: any) {
       {(show as Series).episodes?.map((episode) => (
         <ShowRow
           key={episode.recordingId}
-          episodeID={episode.recordingId}
           show={{ ...show, ...episode }} />
       ))}
     </React.Fragment>
@@ -69,22 +74,6 @@ function IconCell(props: any) {
     <TableCell {...props}>
       <img src={imageFile} style={style} alt="" />
     </TableCell>
-  );
-}
-
-function RecordedOnCell(props: any) {
-  const { show } = props;
-  const recordingDate = recordedOn(show)?.toLocaleDateString("en-US", {
-    dateStyle: "medium"
-  });
-  const recordingTime = recordedOn(show)?.toLocaleTimeString("en-US", {
-    timeStyle: "short"
-  });
-
-  return (
-    <React.Fragment>
-      <TableCell {...props}>{recordingDate} {recordingTime}</TableCell>
-    </React.Fragment>
   );
 }
 
@@ -108,4 +97,53 @@ function DescriptionCell(props: any) {
     default:
       return (<TableCell {...props} style={{ whiteSpace: 'normal' }} />);
   }
+}
+
+function RecordedOnCell(props: any) {
+  const { show } = props;
+  const recordingDate = recordedOn(show)?.toLocaleDateString("en-US", {
+    dateStyle: "medium"
+  });
+  const recordingTime = recordedOn(show)?.toLocaleTimeString("en-US", {
+    timeStyle: "short"
+  });
+
+  return (
+    <React.Fragment>
+      <TableCell {...props}>{recordingDate} {recordingTime}</TableCell>
+    </React.Fragment>
+  );
+}
+
+const actionCellStyle = makeStyles((theme: any) => ({
+  iconButton: {
+    backgroundColor: '#ffffff', // set the background color of the icon button
+    borderRadius: '50%', // make the icon button circular
+    padding: theme.spacing(2), // increase the padding of the icon button
+  },
+}));
+
+function ActionCell(props: any) {
+  const { show } = props;
+  const classes = useStyles();
+
+  var icon
+  switch (show.kind) {
+    case ShowKind.Movie:
+    case ShowKind.Episode:
+      icon = (<DownloadSharpIcon/>)
+      break
+    default:
+      icon = (<UnfoldMoreSharpIcon/>)
+  }
+
+  return (
+    <React.Fragment>
+      <TableCell {...props}>
+        <IconButton size="large" className={classes.iconButton}>
+          {icon}
+        </IconButton>
+      </TableCell>
+    </React.Fragment>
+  );
 }
