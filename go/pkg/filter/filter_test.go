34d14d0016fdbb670230a17c51d9d4803e0609c6
Tom <tartale.work@gmail.com>
2023-08-21 10:50:04 -0600

    Added negative test cases

--- go/pkg/filter/filter_test.go
+++ go/pkg/filter/filter_test.go
@@ -10,15 +10,15 @@ import (
 	"github.com/tartale/kmttg-plus/go/pkg/model"
 )
 
-var _ = Describe("Filter", func() {
+var _ = Describe("Filtering", func() {
 
-	Context("legal filters", func() {
+	Context("for syntactically correct filters", func() {
 		movie := &model.Movie{
 			Kind:  model.ShowKindMovie,
 			Title: "Back to the Future",
 		}
 
-		DescribeTable("can be evaluated as expressions",
+		DescribeTable("can be evaluated against an input that should return true",
 			func(showFiltersJson string, show model.Show) {
 
 				var showFilters []*model.ShowFilter
@@ -50,6 +50,42 @@ var _ = Describe("Filter", func() {
 
 			Entry("multi-field nested filter",
 				`[{"kind": {"eq": "SERIES"}}, {"or": [{"kind": {"eq": "MOVIE"}}, {"and": [{"title": {"eq": "Back to the Future"}}]}]}]`,
+				movie,
+			),
+		)
+
+		DescribeTable("can be evaluated against an input that should return false",
+			func(showFiltersJson string, show model.Show) {
+
+				var showFilters []*model.ShowFilter
+				err := json.Unmarshal([]byte(showFiltersJson), &showFilters)
+				Expect(err).ToNot(HaveOccurred())
+
+				expression := filter.GetExpression(showFilters)
+				values := filter.GetValues(showFilters, show)
+				eval, err := gval.Evaluate(expression, values)
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(eval.(bool)).To(Equal(false))
+			},
+
+			Entry("simple filter",
+				`[{"kind": {"eq": "SERIES"}}]`,
+				movie,
+			),
+
+			Entry("multi-field filter with implied logical 'and'",
+				`[{"kind": {"eq": "SERIES"}}, {"title": {"matches": "Back to the .*"}}]`,
+				movie,
+			),
+
+			Entry("multi-field filter with explicit logical 'or'",
+				`[{"kind": {"eq": "SERIES"}}, {"or": [{"title": {"matches": ".*Shawshank.*"}}]}]`,
+				movie,
+			),
+
+			Entry("multi-field nested filter",
+				`[{"kind": {"eq": "SERIES"}}, {"or": [{"kind": {"eq": "MOVIE"}}, {"and": [{"title": {"eq": "The Shawshank Redemption"}}]}]}]`,
 				movie,
 			),
 		)
