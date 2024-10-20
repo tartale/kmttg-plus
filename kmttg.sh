190efe9305a2bfb0adc6e2d15c87ace73bd9a05c
Tom <tartale.work@gmail.com>
2024-10-19 22:22:40 -0600

    Fix for issue #1; now can use docker-compose up

--- kmttg.sh
+++ kmttg.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail
 
-THIS_SCRIPT_DIR="$(cd $(dirname ${BASH_SOURCE}); pwd)"
+THIS_SCRIPT_DIR="$(cd $(dirname ${BASH_SOURCE[0]}); pwd)"
 
 usage() {
   echo "
@@ -15,13 +15,11 @@ Flags                           Purpose
 
 Environment variable            Purpose
 APP_DIR
-MOUNT_DIR
-TOOLS_DIR
-
 INPUT_DIR
 OUTPUT_DIR
+TOOLS_DIR
+
 COMSKIP_FILE
-ENCODER_DIR
 ENCODER_NAME
 
 " >&2
@@ -32,8 +30,8 @@ ENCODER_NAME
 
 function removeOverriddenEntries() {
 
-  inputPath="${1}"
-  outputPath="${2}"
+  local inputPath="${1}"
+  local outputPath="${2}"
 
   # https://stackoverflow.com/a/56977026/1258206
   #   Iterate through the file, only keep the first <xyz> entry that we see
@@ -62,9 +60,9 @@ function removeOverriddenEntries() {
 }
 
 function mergeIniFiles() {
-  basePath="${1}"
-  overridesPath="${2}"
-  outputPath="${3}"
+  local basePath="${1}"
+  local overridesPath="${2}"
+  local outputPath="${3}"
 
   if [[ -e "${overridesPath}" ]]; then
     export OVERRIDES=$(envsubst < "${overridesPath}")
@@ -72,20 +70,17 @@ function mergeIniFiles() {
     unset OVERRIDES
   fi
 
-  tmpOutputPath="${TMPDIR}/merged.ini"
+  local tmpOutputPath="${TMPDIR}/merged.ini"
   envsubst < "${basePath}" > "${tmpOutputPath}"
   removeOverriddenEntries "${tmpOutputPath}" "${outputPath}"  
 }
 
-if [[ -z "${APP_DIR+x}" ]] || [[ -z "${MOUNT_DIR+x}" ]] || [[ -z "${TOOLS_DIR+x}" ]]; then
+if [[ -z "${APP_DIR+x}" ]] || [[ -z "${INPUT_DIR+x}" ]] || [[ -z "${OUTPUT_DIR+x}" ]] || [[ -z "${TOOLS_DIR+x}" ]]; then
   usage
 fi
 
 export TMPDIR="${TMPDIR:-${PWD}/tmp}"
-export OUTPUT_DIR="${OUTPUT_DIR:-${MOUNT_DIR}/output}"
-export INPUT_DIR="${INPUT_DIR:-${MOUNT_DIR}/input}"
 export COMSKIP_FILE="${COMSKIP_FILE:-comskip.ini.us-ota}"
-export ENCODER_DIR="${ENCODER_DIR:-${INPUT_DIR}/encoders}"
 export ENCODER_NAME="${ENCODER_NAME:-none}"
 if [[ "${ENCODER_NAME}" == "none" ]]; then
   export ENCODE=0
@@ -95,10 +90,10 @@ fi
 
 umask 000
 
-echo "creating required files/directories in ${MOUNT_DIR}"
+echo "creating required files/directories"
 mkdir -p "${TMPDIR}"
 mkdir -p "${INPUT_DIR}/files"
-mkdir -p "${ENCODER_DIR}"
+mkdir -p "${INPUT_DIR}/encoders"
 mkdir -p "${OUTPUT_DIR}/download"
 mkdir -p "${OUTPUT_DIR}/mpeg"
 mkdir -p "${OUTPUT_DIR}/encode"
@@ -106,22 +101,23 @@ mkdir -p "${OUTPUT_DIR}/qsfix"
 mkdir -p "${OUTPUT_DIR}/webcache"
 mkdir -p "${OUTPUT_DIR}/mpegcut"
 mkdir -p "${OUTPUT_DIR}/logs"
+
 touch "${OUTPUT_DIR}/logs/auto.history"
 touch "${OUTPUT_DIR}/logs/auto.log.0"
 
 echo "merging configuration base and overrides files"
-mergeIniFiles "${THIS_SCRIPT_DIR}/config.ini.base" "${INPUT_DIR}/config.ini.overrides" "${INPUT_DIR}/config.ini"
-mergeIniFiles "${THIS_SCRIPT_DIR}/auto.ini.base" "${INPUT_DIR}/auto.ini.overrides" "${INPUT_DIR}/auto.ini"
+mergeIniFiles "${INPUT_DIR}/config.ini.base" "${INPUT_DIR}/config.ini.overrides" "${OUTPUT_DIR}/config.ini"
+mergeIniFiles "${INPUT_DIR}/auto.ini.base" "${INPUT_DIR}/auto.ini.overrides" "${OUTPUT_DIR}/auto.ini"
 
-echo "linking input/output files to app home directory"
-ln -f -s "${INPUT_DIR}/config.ini" "${APP_DIR}/config.ini"
-ln -f -s "${INPUT_DIR}/auto.ini" "${APP_DIR}/auto.ini"
-ln -f -s "${APP_DIR}/${COMSKIP_FILE}" "${APP_DIR}/comskip.ini"
+echo "linking output files to app home directory"
+ln -f -s "${OUTPUT_DIR}/config.ini" "${APP_DIR}/config.ini"
+ln -f -s "${OUTPUT_DIR}/auto.ini" "${APP_DIR}/auto.ini"
 ln -f -s "${OUTPUT_DIR}/logs/auto.history" "${APP_DIR}/auto.history"
 ln -f -s "${OUTPUT_DIR}/logs/auto.log.0" "${APP_DIR}/auto.log.0"
 
-if [[ -e "${ENCODER_DIR}/${ENCODER_NAME}.enc" ]]; then
-  ln -f -s "${ENCODER_DIR}/${ENCODER_NAME}.enc" "${APP_DIR}/encode/"
+ln -f -s "${APP_DIR}/${COMSKIP_FILE}" "${APP_DIR}/comskip.ini"
+if [[ -e "${INPUT_DIR}/encoders/${ENCODER_NAME}.enc" ]]; then
+  ln -f -s "${INPUT_DIR}/encoders/${ENCODER_NAME}.enc" "${APP_DIR}/encode/"
 fi
 
 echo "running kmttg"
