5643456991c14533492129c2b830c8153ca464b8
Tom <tartale.work@gmail.com>
2026-05-07 16:51:37 -0600

    fix tivo filter; move show filter

--- go/pkg/shows/arguments.go
+++ go/pkg/shows/arguments.go
@@ -6,40 +6,9 @@ import (
 	"fmt"
 
 	"github.com/tartale/go/pkg/errorz"
-	"github.com/tartale/go/pkg/filter"
-	"github.com/tartale/go/pkg/generics"
 	"github.com/tartale/go/pkg/gqlgen"
 	"github.com/tartale/kmttg-plus/go/pkg/apicontext"
-	"github.com/tartale/kmttg-plus/go/pkg/model"
 )
-
-func NewFilters(ctx context.Context) ([]*model.ShowFilter, error) {
-	val, err := gqlgen.GetArgValue[[]*model.ShowFilter](ctx, apicontext.ShowFiltersKey)
-	if err != nil && errors.Is(err, generics.ErrNotCasted) {
-		return nil, fmt.Errorf("'%s'; expected type: %s: %w", apicontext.ShowFiltersKey.Name, "[ShowFilter]", errorz.ErrInvalidArgument)
-	}
-	if err != nil && errors.Is(err, errorz.ErrNotFound) {
-		return nil, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return *val, nil
-}
-
-func NewFilterFn(sf []*model.ShowFilter) model.ShowFilterFn {
-	return func(s model.Show) bool {
-		if len(sf) == 0 {
-			return true
-		}
-		expression := filter.GetExpression(sf)
-		values := filter.GetValues(sf, s)
-		eval := filter.MustEvaluate(expression, values)
-
-		return eval.(bool)
-	}
-}
 
 func GetImageDimensions(ctx context.Context) (*apicontext.ImageDimensions, error) {
 	var result apicontext.ImageDimensions
--- go/pkg/tivos/filters.go
+++ go/pkg/tivos/filters.go
@@ -1,13 +1,8 @@
 package tivos
 
 import (
-	"github.com/PaesslerAG/gval"
 	"github.com/tartale/go/pkg/filter"
-	"github.com/tartale/go/pkg/maps"
-	"github.com/tartale/go/pkg/structs"
-	"github.com/tartale/kmttg-plus/go/pkg/logz"
 	"github.com/tartale/kmttg-plus/go/pkg/model"
-	"go.uber.org/zap"
 )
 
 type FilterFn = func(t *model.Tivo) bool
@@ -18,14 +13,8 @@ func NewFilterFn(tf []*model.TivoFilter) FilterFn {
 			return true
 		}
 		expression := filter.GetExpression(tf)
-		structWrapper := structs.New(t)
-		structWrapper.TagName = "json"
-		values := maps.CastPrimitives(structWrapper.Map())
-		eval, err := gval.Evaluate(expression, values)
-		if err != nil {
-			logz.Logger.Warn("error attempting to filter tivos", zap.Error(err))
-			return true
-		}
+		values := filter.GetValues(tf, t)
+		eval := filter.MustEvaluate(expression, values)
 
 		return eval.(bool)
 	}
