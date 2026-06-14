c408e15ac19bcae07fdf1ccdb5481eff0fb65922
tartale <tartalework@gmail.com>
2026-06-14 10:50:30 -0600

    WIP

--- .claude/sandbox/build.sh
+++ .claude/sandbox/build.sh
@@ -2,4 +2,5 @@
 
 THIS_SCRIPT_DIR="$(cd $(dirname ${BASH_SOURCE}); pwd)"
 
-PLUGINS="${THIS_SCRIPT_DIR}/plugin.sh" CS_IMAGE_TAG=kmttg /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/tartale/claude-sandbox/refs/heads/main/build-image.sh)"
+PLUGINS="${THIS_SCRIPT_DIR}/plugin.sh" CS_IMAGE_TAG=kmttg \
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/tartale/claude-sandbox/refs/heads/main/build-image.sh)"
